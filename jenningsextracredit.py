a2288c607a5ca5f012d2fb55c1693c7501da6e7f
BraydenYates <braydengyates@aol.com>
2013-12-19 20:54:21 -0600

    Create jenningsextracredit.py

---
+++ jenningsextracredit.py
@@ -0,0 +1,40 @@
+import os, sys
+
+def congrats():
+        print("                                                        ,jf")
+        print("   _am,    ,_am,  ,_g_oam,    _am,   _g_ag,   _am,   koewkovg   _mm_")
+        print(" ,gF  @._-gF   @-'  jf   @  ,gF  @  ^ NX  #_,gF  @     jf      qK  '")
+        print(" 8Y      8Y    d   j#   jF .8Y  ,d   dY     8Y   d    jf       *b,")
+        print("jK   ,  jK   ,N   jN   jF  :K  ,Z  ,jF     jK  ,Z'  ,jfk,       dN.")
+        print(" NbpP    NbpP    dP   dFk_o8NbpP'V^dF       NbpY'V^'dF 'dYo-'*h,W'")
+        print("                         ,gF',@'")
+        print("                        :8K  j8")
+        print("                         '*w*'")
+
+
+print("Algebra Extra Credit \n ")
+
+print("When answering, please format it as: \n x=number \n No spaces, please. \n")
+ans = raw_input("Solve. \n 5x+2x=21 \n")
+if ans == "x=3":
+        
+        print ("Correct. Continuing. \n")
+        ans = raw_input("Simplify. \n 15x+7y+2x \n")
+        
+        if ans == "17x+7y":
+                print( "Correct. Continuing. \n")
+                
+                ans = raw_input("Simplify. \n 4k-3l-2k \n")
+                if ans == "2k-31":
+                        print("Correct. Continuing. \n")
+                        ans = raw_input("Solve the following. \n 38=2a+54 \n")
+                        if ans == "a=-8":
+                                print("Correct.")
+                                ans = raw_input("You payed $600 for a new guitar.  Your guitar \n costs $40 more  than twice the cost  of your friend's \n guitar. How much did your friend's guitar cost? \n")
+                                if ans == "$280":
+                                        print("Correct. Continuing.")
+                                        ans = raw_input("Solve. \n t+18.1=23.9 \n")
+                                        if ans == "t=5.8":
+                                                print("Correct. Continuing. ")
+                                                congrats()
+                                
